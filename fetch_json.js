424785e264dc6948017858278a83cc78e8652b26
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-05-30 01:38:23 -0400

    Update fetch_json.js

--- fetch_json.js
+++ fetch_json.js
@@ -40,45 +40,4 @@ export async function fetchJSON() {
         
         list.appendChild(card);
     });
-}import { getThumbnailFromId } from "./utils.js";
-
-const files = [
-    "./levels/tmkocx.json",
-    "./levels/tmkoc.json",
-    "./levels/tmkoc_og.json",
-];
-
-export async function fetchJSON() {
-
-    const list = document.getElementById("list");
-
-    if (!list) {
-        console.error("list element not found");
-        return;
-    }
-
-    const responses = await Promise.all(
-        files.map(file => fetch(file))
-    );
-
-    const data = await Promise.all(
-        responses.map(r => r.json())
-    );
-
-    data.forEach(item => {
-
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
-
-            <img src="${getThumbnailFromId(item.verification)}">
-        <div class="card-text">
-            <h3>.rank - ${item.name}</h3>
-            <h4><strong>Creator:</strong> ${item.creator}</h4>
-            <h4><strong>Verifier:</strong> ${item.verifier}</h4>
-        </div>
-        `;
-        
-        list.appendChild(card);
-    });
 }
