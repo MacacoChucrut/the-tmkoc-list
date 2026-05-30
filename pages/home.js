3ead002ea932214934b142463846d149cde5659b
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-05-30 01:35:14 -0400

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -3,11 +3,45 @@ export function renderHome(content) {
 
     <div class="home">
     <h2>WELCOME TO THE TMKOC LIST</h2>
-    <h2>🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳</h2>
+    <h2>🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳 🇮🇳</h2>
 
+    <div class="home-gifs">
     <img src="assets/gifs/dance.gif">
     <img src="assets/gifs/fine.gif">
     <img src="assets/gifs/dance2.gif">
+    </div>
+
+    <div class="level-rules">
+    <h2>LEVEL RULES</h2>
+    <ol>
+    <li>Levels must have atleast one gameplay parts from the start of <strong>tmkoc OG.</strong></li>
+    <li>Level names must contain references to the show, such as character names, or the show itself.</li>
+    <li>Levels must be at least 30 seconds long.</li>
+    <li>2P levels are NOT allowed.</li>
+    <li>Copying parts from a level outside the list is NOT allowed.</li>
+    <li>Levels requiring more than 15 clicks per second are NOT allowed (The level must be REALISTICALLY possible under 15 CPS, meaning that you can't make it so it's borderline impossible with 15 CPS but easy with a higher amount).</li>
+    <li>Anything using the Random Trigger must not affect the gameplay or visual difficulty.</li>
+    </ol>
+    </div>
+
+    <div class="SUBMISSION REQUIREMENTS">
+    <h2>SUBMISSION REQUIREMENTS</h2>
+    <ol>
+    <li>Verifications must be uploaded in the format of a YouTube video.</li>
+    <li>The recording must have click/tap sounds, being fully audible throughout the entire level.</li>
+    <li>A Cheat Indicator is required if a modmenu with the feature is being used.</li>
+    <li>The recording must have a previous attempt showing the death animation before the completion, unless the completion is on the first attempt.</li>
+    <li>The recording must show the "Level Complete!" screen at the end.</li>
+    <li>Do NOT use major secret ways of bug routes</li>
+    <li>You may not use any disallowed mods. A list of what we allow and don't allow can be found <a href="https://docs.google.com/spreadsheets/d/1M4vXMxHcYwtstB6SD9r4lPFotUXhz3IL9D_3JX8tjyE/edit?gid=1204643762#gid=1204643762">here.</a></li>
+    </ol>
+    
+    <div class="home-gifs-2">
+    <img src="assets/gifs/shocked.gif">
+    <img src="assets/gifs/slap.gif">
+    <img src="assets/gifs/confused.gif">
+    </div>
+
     </div>
     `;
 }
