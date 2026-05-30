b155d0f86b52c7cee8473b2ff501075861b6933e
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-05-30 01:34:22 -0400

    Update fetch_json.js

--- fetch_json.js
+++ fetch_json.js
@@ -28,6 +28,48 @@ export async function fetchJSON() {
         const card = document.createElement("div");
         card.className = "card";
         card.innerHTML = `
+        
+        <a href="${item.verification}" target="_blank">
+        <img src="${getThumbnailFromId(item.verification)}"></a>
+        <div class="card-text">
+        <h3>${item.name}</h3>
+        <h4>Creator: ${item.creator}</h4>
+        <h4>Verifier: ${item.verifier}</h4>
+        </div>
+        `;
+        
+        list.appendChild(card);
+    });
+}import { getThumbnailFromId } from "./utils.js";
+
+const files = [
+    "./levels/tmkocx.json",
+    "./levels/tmkoc.json",
+    "./levels/tmkoc_og.json",
+];
+
+export async function fetchJSON() {
+
+    const list = document.getElementById("list");
+
+    if (!list) {
+        console.error("list element not found");
+        return;
+    }
+
+    const responses = await Promise.all(
+        files.map(file => fetch(file))
+    );
+
+    const data = await Promise.all(
+        responses.map(r => r.json())
+    );
+
+    data.forEach(item => {
+
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
 
             <img src="${getThumbnailFromId(item.verification)}">
         <div class="card-text">
