797b7f96ced641071e9fe1a72f94aec75fe88f05
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-05-31 12:50:09 -0400

    Update fetch_json.js

--- fetch_json.js
+++ fetch_json.js
@@ -1,43 +1,44 @@
 import { getThumbnailFromId } from "./utils.js";
 
-const files = [
-    "./levels/tmkocx.json",
-    "./levels/tmkoc.json",
-    "./levels/tmkoc_og.json",
-];
-
 export async function fetchJSON() {
 
     const list = document.getElementById("list");
 
-    if (!list) {
-        console.error("list element not found");
-        return;
-    }
+    list.innerHTML = "";
 
-    const responses = await Promise.all(
-        files.map(file => fetch(file))
+    const listResponse = await fetch("./levels/_list.json");
+    const levelNames = await listResponse.json();
+    const responses = await Promise.all(levelNames.map(id => fetch(`./levels/${id}.json`)
+        )
     );
 
     const data = await Promise.all(
         responses.map(r => r.json())
     );
 
-    data.forEach(item => {
+    data.forEach((item, index) => {
 
         const card = document.createElement("div");
         card.className = "card";
+        card.onclick = () => {
+            location.hash = `#level/${item.id}`;
+        };
         card.innerHTML = `
-        
+
         <a href="${item.verification}" target="_blank">
         <img src="${getThumbnailFromId(item.verification)}"></a>
-        <div class="card-text">
-        <h3>${item.name}</h3>
-        <h4>Creator: ${item.creator}</h4>
-        <h4>Verifier: ${item.verifier}</h4>
-        </div>
-        `;
         
+        <div class="card-text">
+        <h3>#${index + 1} - ${item.name}</h3>
+        <h4>Creator(s): ${item.creator}</h4>
+        <h4>Verifier: ${item.verifier}</h4></div>
+        `;
+
+        const link = card.querySelector("a");
+        link.addEventListener("click", (event) => {
+            event.stopPropagation();
+        });
+
         list.appendChild(card);
     });
 }
